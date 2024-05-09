a467a0c8cd9185fbe4d9426019832dd514264a4a
moonditch <156866717+moonditch@users.noreply.github.com>
2024-05-09 14:59:33 +0800

    试图解决ui掉帧问题

--- GlobalTranslation/StatsTranslate.cs
+++ GlobalTranslation/StatsTranslate.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using Terraria.ModLoader;
 using Terraria.UI.Chat;
-using ThoriumMod.UI;
 
 namespace ThoriumModzhcn.GlobalTranslation
 {
@@ -16,25 +15,20 @@ namespace ThoriumModzhcn.GlobalTranslation
         public override void Unload()
         {
             On_ChatManager.ParseMessage -= Translate;
-        }       
+        }      
+ 
         private static List<TextSnippet> Translate(On_ChatManager.orig_ParseMessage orig, string text, Color baseColor)
         {
             //原瑟银
-           /* text = Regex.Replace(text, "Healer Stats", "牧师属性");
-            text = Regex.Replace(text, "Current Bonus Healing", "当前额外治疗量");
-            text = Regex.Replace(text, "Best Heal Streak", "最大连击治疗量");
-            text = Regex.Replace(text, "Healing done", "治疗量");
-            text = Regex.Replace(text, "Damage mitigated", "伤害减缓");
-            text = Regex.Replace(text, "Lives saved", "救死扶伤");
-            text = Regex.Replace(text, "Close calls", "死里逃生");
+            text = Regex.Replace(text, "Healer Stats", "牧师属性");
+            text = Regex.Replace(text, "(Current Bonus Healing: )([0-9]+)\n(Best Heal Streak: )([0-9]+)\n(Healing done: )([0-9]+)\n(Damage mitigated: )([0-9]+)\n(Lives saved: )([0-9]+)\n(Close calls: )([0-9]+)", "当前额外治疗量: $2\n最大连击治疗量: $4\n治疗量: $6\n伤害减缓: $8\n救死扶伤: $10\n死里逃生: $12");
             text = Regex.Replace(text, "Bard Stats", "吟游诗人属性");
             text = Regex.Replace(text, "Bonus Duration", "咒音增幅的持续时间");
             text = Regex.Replace(text, "sec\\(s\\)", "秒");
             text = Regex.Replace(text, "Max-Use Duration", "最大使用时间");
-            text = Regex.Replace(text, "Base Inspiration", "基础灵感值");
-            text = Regex.Replace(text, "Bonus Inspiration", "额外灵感值");
-            text = Regex.Replace(text, "Timed Hits", "抓住节拍次数");
-            text = Regex.Replace(text, "sec", "秒"); */
+            text = Regex.Replace(text, "sec", "秒");
+            text = Regex.Replace(text, "(Base Inspiration: )([0-9]+)( / )([0-9]+)\n(Bonus Inspiration: )([0-9]+)\n(Timed Hits: )([0-9]+)", "基础灵感值: $2 / $4\n额外灵感值: $6\n抓住节拍次数: $8");
+            text = Regex.Replace(text, "(Total: )([0-9]+)(% Damage Reduction\nVanilla: )([0-9]+)(%\nThorium: )([0-9]+)(%)", "合计: $2% 伤害减免\n原版: $4%\n瑟银: $6%");
 
 
             //瑟银重置
