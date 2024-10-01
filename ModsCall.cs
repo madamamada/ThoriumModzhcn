be2451f219f4445312c9997447142ecb11bf2f1a
moonditch <156866717+moonditch@users.noreply.github.com>
2024-10-02 07:54:59 +0800

    同步瑟银汉化测试版2014.10.1

---
+++ ModsCall.cs
@@ -0,0 +1,20 @@
+﻿using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace ThoriumModzhcn
+{
+	internal static class ModsCall
+	{
+		internal static Mod Thorium
+		{
+			get
+			{
+				if (!ModLoader.TryGetMod("ThoriumMod", out Mod thorium))
+				{
+					return null;
+				}
+				return thorium;
+			}
+		}
+	}
+}
