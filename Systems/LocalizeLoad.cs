995bd9f03371b05c239eee68931bb174d3a55dbc
moonditch <156866717+moonditch@users.noreply.github.com>
2025-03-07 10:38:27 +0800

    瑟银简转台繁/港繁测试

---
+++ Systems/LocalizeLoad.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Config;
+using Terraria.ModLoader.Core;
+
+namespace ThoriumModzhcn.Systems
+{
+    public class LocalizeLoad : ModSystem
+    {
+        public static List<string> HKHJson { get; } =
+         [
+            "Localization/zh-HK/zh-hk.hjson",
+            "Localization/zh-HK/zh-hk_Mods.CalamityBardHealer.hjson",
+            "Localization/zh-HK/zh-hk_Mods.RedemptionBardHealer.hjson",
+            "Localization/zh-HK/zh-hk_Mods.SOTSBardHealer.hjson",
+            "Localization/zh-HK/zh-hk_Mods.SpiritBardHealer.hjson",
+            "Localization/zh-HK/zh-hk_Mods.SpookyBardHealer.hjson",
+            "Localization/zh-HK/zh-hk_Mods.TerrariumHacks.hjson",
+            "Localization/zh-HK/zh-hk_Mods.ThoriumRework.hjson",
+            "Localization/zh-HK/zh-hk_Mods.UnlimitedBardEnergy.hjson"
+        ];
+        public static List<string> TWHJson { get; } =
+         [
+            "Localization/zh-TW/zh-tw.hjson",
+            "Localization/zh-TW/zh-tw_Mods.CalamityBardHealer.hjson",
+            "Localization/zh-TW/zh-tw_Mods.RedemptionBardHealer.hjson",
+            "Localization/zh-TW/zh-tw_Mods.SOTSBardHealer.hjson",
+            "Localization/zh-TW/zh-tw_Mods.SpiritBardHealer.hjson",
+            "Localization/zh-TW/zh-tw_Mods.SpookyBardHealer.hjson",
+            "Localization/zh-TW/zh-tw_Mods.TerrariumHacks.hjson",
+            "Localization/zh-TW/zh-tw_Mods.ThoriumRework.hjson",
+            "Localization/zh-TW/zh-tw_Mods.UnlimitedBardEnergy.hjson"
+        ];
+
+        public static List<string> ZHHJson { get; } = 
+        [
+            "Localization/zh-Hans/zh-Hans.hjson",
+            "Localization/zh-Hans/zh-Hans_Mods.CalamityBardHealer.hjson",
+            "Localization/zh-Hans/zh-Hans_Mods.RedemptionBardHealer.hjson",
+            "Localization/zh-Hans/zh-Hans_Mods.SOTSBardHealer.hjson",
+            "Localization/zh-Hans/zh-Hans_Mods.SpiritBardHealer.hjson",
+            "Localization/zh-Hans/zh-Hans_Mods.SpookyBardHealer.hjson",
+            "Localization/zh-Hans/zh-Hans_Mods.TerrariumHacks.hjson",
+            "Localization/zh-Hans/zh-Hans_Mods.ThoriumRework.hjson",
+            "Localization/zh-Hans/zh-Hans_Mods.UnlimitedBardEnergy.hjson"
+        ];
+
+        public override void Load()
+        {
+            //var r = (TmodFile)typeof(Mod).GetProperty("File", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(Mod);
+            //foreach (var item in r) {
+            //    var file = item;
+            //}
+
+            Load(HKHJson, LocalizeUtil.Language.香港繁体);
+            Load(TWHJson, LocalizeUtil.Language.台湾繁体);
+            Load(ZHHJson, LocalizeUtil.Language.简体中文);
+            base.Load();
+        }
+
+        static LocalizeLoad()
+        {
+        }
+
+        private static void Load(List<string> paths, LocalizeUtil.Language language)
+        {
+            foreach (var hjson in paths) {
+                LocalizeUtil.LoadLocalizedKey(hjson, language, GetStartChars(hjson));
+            }
+            Dictionary<string, string> localizeTests = LocalizeUtil.GetLocalizationDictionary(language);
+            string regex = @"\{\$(.*?)\}";
+            foreach (var kv in localizeTests) {
+                MatchCollection matchs = Regex.Matches(kv.Value, regex);
+                foreach (Match item in matchs) {
+                    string @waibu = item.Groups[0].Value; //0是原始值，1是内部值
+                    string @neibu = item.Groups[1].Value;
+                    string[] start = kv.Key.Split(".");
+                    string keyStart = start[0] + "." + start[1] + "."; //Key的开头
+                    string key = keyStart + neibu;       //父字符串的本地化Key
+                    if (localizeTests.TryGetValue(key, out string frstring)) {//父字符串({$})的值
+                        localizeTests[kv.Key] = kv.Value.Replace(waibu, frstring);
+                    }
+                }
+            }
+        }
+
+        private static string GetStartChars(string fileName)
+        {
+            var splitString = fileName.Split("_");
+            if (splitString.Length == 1) return "";
+            if (splitString[0] == "IL") return "";
+            return splitString[1].Replace(".hjson", ".");
+        }
+    }
+
+    public class 语言切换 : ModConfig
+    {
+        public LocalizeUtil.Language 语言;
+
+        public override ConfigScope Mode => ConfigScope.ClientSide;
+
+        public override void OnChanged()
+        {
+            LocalizeUtil.CutLanguage(语言);
+            base.OnChanged();
+        }
+    }
+}
---
+++ Systems/LocalizeUtil.cs
@@ -0,0 +1,267 @@
+﻿using Hjson;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace ThoriumModzhcn.Systems
+{
+    public class LocalizeUtil : ModSystem
+    {
+        public enum Language
+        {
+            简体中文,
+            台湾繁体,
+            香港繁体
+        }
+
+        static LocalizeUtil()
+        {
+            Init();
+        }
+
+        private static Dictionary<string, string> 台湾繁体 { get; } = [];
+        private static Dictionary<string, string> 香港繁体 { get; } = [];
+        private static Dictionary<string, string> 简体中文 { get; } = [];
+        //private static Dictionary<string, LocalizedText> TaiWan { get; } = [];
+        //private static Dictionary<string, LocalizedText> HongKong { get; } = [];
+        //private static Dictionary<string, LocalizedText> Chinese { get; } = [];
+
+
+        private static bool IsInit = false;
+
+        /// <summary>
+        /// 设置LocalizedText值的方法
+        /// </summary>
+        public static MethodInfo LocalizedTextSetValue { get; private set; }
+
+        /// <summary>
+        /// 全部本地化键和值
+        /// </summary>
+        public static Dictionary<string, LocalizedText> LocalizedTexts { get; private set; }
+
+        /// <summary>
+        /// LocalizedText的构造方法(私有的)
+        /// </summary>
+        private static ConstructorInfo LocalizedTextCons { get; set; }
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        internal static void Init()
+        {
+            if (IsInit) return;
+            LocalizedTextSetValue = typeof(LocalizedText).GetProperty(nameof(LocalizedText.Value)).GetSetMethod(true);
+            LocalizedTexts = (Dictionary<string, LocalizedText>)typeof(LanguageManager).GetField("_localizedTexts", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(LanguageManager.Instance);
+            LocalizedTextCons = typeof(LocalizedText).GetConstructor(BindingFlags.NonPublic | BindingFlags.Instance, [typeof(string), typeof(string)]);
+            IsInit = true;
+        }
+
+        /// <summary>
+        /// 创建一个LocalizedText，其构造方法是私有的
+        /// </summary>
+        public static LocalizedText CreateLocalizedText(string Key, string Value)
+        {
+            return (LocalizedText)LocalizedTextCons.Invoke([Key, Value]);
+        }
+
+        /// <summary>
+        /// 一步到位 传入文件名称,返回键值
+        /// <para>例: zh-tw_Mods.LocalText.hjson</para>
+        /// </summary>
+        public static List<(string, string)> OneStepToGo(string fileName, string startChar = "") => Method(GetText(fileName), startChar, fileName);
+
+        /// <summary>
+        /// 加载文件键值到字典
+        /// </summary>
+        public static void LoadLocalizedKey(string fileName, Language language, string startChar = "")
+        {
+            Dictionary<string, string> keyValue;
+            //Dictionary<string, LocalizedText> keyValue;
+            keyValue = GetLocalizationDictionary(language);
+            if (keyValue == null) return;
+            foreach (var item in OneStepToGo(fileName, startChar)) {
+                if (!keyValue.ContainsKey(item.Item1)) {
+                    keyValue.Add(item.Item1, /*CreateLocalizedText(item.Item1, item.Item2)*/item.Item2);
+                    if (!LocalizedTexts.ContainsKey(item.Item1))
+                        LocalizedTexts.Add(item.Item1, CreateLocalizedText(item.Item1, item.Item2));
+                }
+            }
+        }
+
+        /// <summary>
+        /// 转换到目标语言
+        /// </summary>
+        public static void CutLanguage(Language language)
+        {
+            switch (language) {
+                case Language.台湾繁体:
+                    GoToDictionary(台湾繁体);
+                    break;
+                case Language.香港繁体:
+                    GoToDictionary(香港繁体);
+                    break;
+                case Language.简体中文:
+                    GoToChinese();
+                    GoToDictionary(简体中文);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// 将现在使用的本地化值切换为传入字典的值
+        /// </summary>
+        //private static void GoToDictionary(Dictionary<string, LocalizedText> localizedTexts)
+        private static void GoToDictionary(Dictionary<string, string> localizedTexts)
+        {
+            foreach (var kv in localizedTexts) {
+                if (LocalizedTexts.ContainsKey(kv.Key)) {
+                    //LocalizedTexts[kv.Key] = kv.Value;
+                    var xg = LocalizedTexts[kv.Key];
+                    LocalizedTextSetValue.Invoke(xg, [kv.Value]);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 恢复为中文(只适用于原生Hjson)
+        /// </summary>
+        private static void GoToChinese()
+        {
+            //LocalizationLoader.LoadModTranslations(new GameCulture("zh-Hans", 7));
+        }
+
+        /// <summary>
+        /// 获取语言对应的字典
+        /// </summary>
+        //private static Dictionary<string, LocalizedText> GetLocalizationDictionary(Language language)
+        public static Dictionary<string, string> GetLocalizationDictionary(Language language)
+        {
+            if (language == Language.台湾繁体)
+                return 台湾繁体;
+            else if (language == Language.香港繁体)
+                return 香港繁体;
+            else
+                return 简体中文;
+        }
+
+        /// <summary>
+        /// 传入模组内文件夹名称,返回此文件的内容
+        /// <para> 例: zh-tw_Mods.LocalText.hjson </para>
+        /// </summary>
+        private static string GetText(string fileName)
+        {
+            if (!fileName.EndsWith("hjson"))
+                return string.Empty;
+            //if (FileExists(fileName))
+            //    return string.Empty;
+            if(ModLoader.TryGetMod(typeof(LocalizeUtil).FullName.Split(".")[0], out Mod mod)) {
+                //读取文件
+                using Stream file = mod.GetFileStream(fileName);
+                using StreamReader fileRead = new StreamReader(file);
+                string hjsonValue = fileRead.ReadToEnd();
+                return hjsonValue;
+            }
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// 传入Hjson的内容,解析为键值，这个是TML的方法
+        /// <para> LocalizationLoader.LoadTranslations</para>
+        /// </summary>
+        private static List<(string, string)> Method(string translationFileContents, string startChar = "", string fileName = "")
+        {
+            // Parse HJSON and convert to standard JSON
+            var flattened = new List<(string, string)>();
+            string jsonString;
+            try {
+                jsonString = HjsonValue.Parse(translationFileContents).ToString();
+            } catch (Exception e) {
+                string additionalContext = "";
+                if (e is ArgumentException && Regex.Match(e.Message, "At line (\\d+),") is Match { Success: true } match && int.TryParse(match.Groups[1].Value, out int line)) {
+                    string[] lines = translationFileContents.Replace("\r", "").Replace("\t", "    ").Split('\n');
+                    int start = Math.Max(0, line - 4);
+                    int end = Math.Min(lines.Length, line + 3);
+                    var linesOutput = new StringBuilder();
+                    for (int i = start; i < end; i++) {
+                        if (line - 1 == i)
+                            linesOutput.Append($"\n{i + 1}[c/ff0000:>" + lines[i] + "]");
+                        else
+                            linesOutput.Append($"\n{i + 1}:" + lines[i]);
+                    }
+                    additionalContext = "\nContext:" + linesOutput.ToString();
+                }
+                throw new Exception(/*$"The localization file \"{translationFile.Name}\" is malformed and failed to load:{additionalContext} ", e*/);
+            }
+
+            // Parse JSON
+            var jsonObject = JObject.Parse(jsonString);
+
+            foreach (JToken t in jsonObject.SelectTokens("$..*")) {
+                if (t.HasValues) {
+                    continue;
+                }
+
+                // Due to comments, some objects can by empty
+                if (t is JObject obj && obj.Count == 0)
+                    continue;
+
+                // Custom implementation of Path to allow "x.y" keys
+                string path = "";
+                JToken current = t;
+
+                for (JToken parent = t.Parent; parent != null; parent = parent.Parent) {
+                    path = parent switch
+                    {
+                        JProperty property => property.Name + (path == string.Empty ? string.Empty : "." + path),
+                        JArray array => array.IndexOf(current) + (path == string.Empty ? string.Empty : "." + path),
+                        _ => path
+                    };
+                    current = parent;
+                }
+
+                // removing instances of .$parentVal is an easy way to make this special key assign its value
+                //  to the parent key instead (needed for some cases of .lang -> .hjson auto-conversion)
+
+                ////修改的地方，因为没有完全按照TML的来
+                ////path.Replace(".$parentVal", "")
+                //if(path.StartsWith("Mods"))
+
+                //    flattened.Add((path, t.ToString()));
+                //else if(!path.StartsWith("Mods") && startChar == "")
+                //    throw new Exception("给定文件不对，键没有以Mods.xxxx正确开头，如需要解决这个问题，可以传入 startChar");
+                //else {
+                //    if (startChar.EndsWith("."))
+                //        path = startChar + path.Replace(".$parentVal", "");
+                //    else
+                //        path = startChar + "." + path.Replace(".$parentVal", "");
+                //}
+                #region ?
+                //if (startChar != "")
+                //    path = startChar + path.Replace(".$parentVal", "");
+                //else
+                //    path = path.Replace(".$parentVal", "");
+
+                //var stringSplit = fileName.Split("/");
+                //if (stringSplit.Length == 0) ;
+                //else if (stringSplit.Length != 0)
+                //    fileName = stringSplit[stringSplit.Length - 1];
+                //if (path.StartsWith(fileName))
+                //    path = path.Substring(fileName.Length);
+                #endregion
+                path = startChar + path.Replace(".$parentVal", "");
+                flattened.Add((path, t.ToString()));
+            }
+
+
+            return flattened;
+        }
+
+    }
+}
